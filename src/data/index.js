029d77c9208cd0393f01b4e1d57939f3d6348a8f
Nikita Dumkin <ndumkin@mail.ru>
2023-11-06 11:09:31 +0500

    Store

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+export const dashboardData = {
+    id: 0,
+    name: '',
+    type: '',
+    data: [99, 101, 23, 45, 67, 88],
+};
---
+++ src/pages/Mock/MockPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mock">
+    a
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+
+.mock{
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  border: 5px solid red;
+}
+
+</style>
